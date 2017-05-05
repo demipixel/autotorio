c2ce149e3561f339827ec9f45536fa015c97e3dd
Lucas Simon <luke5227@gmail.com>
2017-05-05 15:27:49 -0700

    Implement saving/loading via url, fix checkbox not having ID

--- assets/js/handleForm.js
+++ assets/js/handleForm.js
@@ -1,35 +1,63 @@
 
 function submitForm(form) {
   try {
-  if ($('.error-block:visible').length > 0 || !$('#blueprint').val()) {
-    BootstrapDialog.show({
-      type: BootstrapDialog.TYPE_DANGER,
-      title: 'Error',
-      message: !$('#blueprint').val() ? 'You must provide a blueprint string!' : 'You need to fix some errors!',
-    });
-  } else {
-    BootstrapDialog.show({
-      title: 'Outpots Blueprint String',
-      message: 'Loading...',
-      onshow: function(dialog) {
-        $.ajax({
-          type: 'POST',
-          url: '/outpost/string',
-          data: $(form).serializeArray(),
-          dataType: 'json',
-          success: function(data) {
-            console.log(data);
-            dialog.getModalBody().html('<textarea class="form-control" rows=10>'+(data.error || data.string)+'</textarea>');
-            setTimeout(function() { dialog.getModalBody().find('textarea').select(); }, 450);
-          },
-          error: function(data) {
-            console.log(data);
-          }
-        });
-      }
-    })
-  }
-  } catch (e) { console.log(e)}
+    if ($('.error-block:visible').length > 0 || !$('#blueprint').val()) {
+      BootstrapDialog.show({
+        type: BootstrapDialog.TYPE_DANGER,
+        title: 'Error',
+        message: !$('#blueprint').val() ? 'You must provide a blueprint string!' : 'You need to fix some errors!',
+      });
+    } else {
+      BootstrapDialog.show({
+        title: 'Outpots Blueprint String',
+        message: 'Loading...',
+        onshow: function(dialog) {
+          $.ajax({
+            type: 'POST',
+            url: '/outpost/string',
+            data: $(form).serializeArray(),
+            dataType: 'json',
+            success: function(data) {
+              console.log(data);
+              dialog.getModalBody().html('<textarea class="form-control" rows=10>'+(data.error || data.string)+'</textarea>');
+            },
+            error: function(data) {
+              console.log(data);
+            }
+          });
+        },
+        onshown: function(dialog) {
+          dialog.getModalBody().find('textarea').select();
+        }
+      })
+    }
+  } catch (e) { console.log(e); }
+}
+
+function getSettingsURL() {
+  var url = document.location.origin + document.location.pathname + '?';
+  FORM_DATA.forEach(function(element) {
+    if (element.name == 'blueprint') return;
+
+    if (element.name) {
+      $e = $('#'+element.name);
+      if ($e.val()) url += element.name+'='+encodeURI($e.val())+'&';
+    }
+
+    if (element.checkbox) {
+      $c = $('#'+element.checkbox.name);
+      url += element.checkbox.name+'='+encodeURI($c.is(':checked'))+'&';
+    }
+  });
+  url = url.slice(0, -1);
+
+  BootstrapDialog.show({
+    title: 'Custom Settings URL',
+    message: '<input class="form-control" value="'+url+'">',
+    onshown: function(dialog) {
+      dialog.getModalBody().find('input').select();
+    }
+  });
 }
 
 $(document).ready(function() {
@@ -37,7 +65,6 @@ $(document).ready(function() {
 
   $trainDirection = $('#trainDirection');
   $trainDirection.change(function() {
-    console.log('Here', $trainDirection.val()== 'Top');
     if ($trainDirection.val() == 'Top' || $trainDirection.val() == 'Bottom') {
       $('#minedOreDirection option').show();
       $('#minedOreDirection').val("Right").find('option[value=Up],[value=Down]').hide();
@@ -48,6 +75,8 @@ $(document).ready(function() {
   });
 
   FORM_DATA.forEach(function(element) {
+    if (getUrlParameter(element.name)) $('#'+element.name).val(getUrlParameter(element.name));
+    if (element.checkbox && getUrlParameter(element.checkbox.name)) $('#'+element.checkbox.name).prop('checked', getUrlParameter(element.checkbox.name) == 'true');
     if (element.type != 'input') return;
 
     $('#'+element.name).keyup(function() {
@@ -68,3 +97,19 @@ $(document).ready(function() {
   });
 });
 
+
+
+function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
