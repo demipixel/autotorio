2c7ed007aa5831bc735e4433b58a5f7c1ef56a30
demipixel <demipixel@users.noreply.github.com>
2018-01-06 21:22:57 -0800

    Update donators list

--- assets/js/generators.js
+++ assets/js/generators.js
@@ -25,7 +25,7 @@ var CONVERT_TILE = {
   'Hazard Concrete Right': 'hazard_concrete_right'
 };
 
-const DONATORS = ['phono\'s', 'Goopsky\'s', 'Fahr', 'Dwarf\'s Foundry', 'Tupper\'s', 'Schorty\'s', 'nerdkusi\'s', 'Autotorio Approved™']; // Okay, last one wasn't technically created by a donator...
+const DONATORS = ['phono\'s', 'Goopsky\'s', 'Fahr', 'Dwarf\'s Foundry', 'Tupper\'s', 'Schorty\'s', 'nerdkusi\'s', 'Geo\'s', 'Autotorio Approved™']; // Okay, last one wasn't technically created by a donator...
 
 function generateOutpost(form) {
   var opt = {};
