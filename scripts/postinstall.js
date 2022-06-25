07711af4de940e051f00dd18e2e6993ba8f403a6
Lucas Simon <luke5227@gmail.com>
2022-06-24 18:30:01 -0700

    Fix build issue, update factorio-generators

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,12 +8,10 @@ if (!fs.existsSync('assets/build')) {
 browserify('node_modules/factorio-generators', {
   standalone: 'factorioGenerators'
 })
-  .transform('babelify', { presets: ['es2015'] })
-  .transform(require('aliasify'), {
-    aliases: {
-      zlib: 'browserify-zlib-next'
-    }
-  })
+  .transform('babelify', { presets: [
+    '@babel/preset-env',
+  ] })
+  .transform(require('aliasify'))
   // .transform({ global: true }, 'uglifyify')
   .bundle()
   .pipe(fs.createWriteStream('assets/build/factorio-generators.js'));
